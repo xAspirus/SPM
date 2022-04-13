01da0b06d5c9929df7d8e999889c915713478c15
aspirus <aspirus@192.168.1.43>
2022-04-13 16:41:11 +0530

    e

--- spm.py
+++ spm.py
@@ -1,144 +1,109 @@
-#!/bin/python3
+import json
+import os
 
 from rich import print as rprint
-import os
-import json
-import sys
+
+AnyPath = str
+OMEGA = 'Ω'
+
+
+class Zip:
+	""" Header for zip/unzip commands """
+	def unzip(in_path: str, out_path: str):
+		os.system(f"unzip -o '{in_path}' -d '{out_path}'")
+	
+	def zip(in_path: str, out_path: str):
+		os.system(f"zip -rj '{out_path}' '{in_path}'")
 
 
 class Project:
-	def __init__(self, name: str):
-		self.name = name
-		self.dir_path = str()
-		self.json = dict()
-	
-	def load_project_dir(self, dir_path: str):
-		if os.path.isfile(f'./{dir_path}/{self.name}.sb3'):
-			pass
-		else:
-			raise Exception(" Invalid project folder ")
-		self.dir_path = dir_path
-		rprint(f"[blue] ** Unzipping {self.name}.sb3 [/blue]")
-		os.system(f"unzip -o ./{self.dir_path}/{self.name}.sb3 -d ./{self.dir_path}/__unpacked__")
-		with open(f"./{self.dir_path}/__unpacked__/project.json", "r") as f:
-			self.json = json.loads(f.read())
+	def __init__(self, sb3_path: AnyPath):
+		self.name = sb3_path.split('/')[-1].replace('.sb3','')
+		self.id = self.name + OMEGA
+		self.sb3_path = sb3_path
+		self.json = {}
 
-	def package(self, output: str = None):
-		if output is None: output = self.name
-		with open(f"./{self.dir_path}/__unpacked__/project.json", "w") as f:
-			f.write(json.dumps(self.json))
-		rprint(f"[blue] ** Zipping project into {output}.sb3 [/blue]")
-		os.system(f"zip -rj ./{self.dir_path}/{output}.sb3 ./{self.dir_path}/__unpacked__")
+		if not os.path.isfile(self.sb3_path):
+			raise FileNotFoundError(f'Project file "{self.sb3_path}" does not exist')
+		Zip.unzip(self.sb3_path, f'./__{self.name}__')
+		with open(f'./__{self.name}__/project.json', 'r') as fp:
+			self.json = json.loads(fp.read())
 	
-	def print_json(self):
-		rprint(f"[green] ** {self.name}'s json contents: [/green]")
-		rprint(self.json)
+	def __del__(self):
+		os.system(f"rm -rf './__{self.name}__'")
+
+	def package_sb3(self, output_file: AnyPath = None):
+		if output_file is None: output_file = self.sb3_path
+		with open(f'./__{self.name}__/project.json', 'w') as fp:
+			fp.write(json.dumps(self.json))
+		Zip.zip(f'./__{self.name}__', output_file)
 	
 	def get_sprite(self, sprite_name: str):
-		for sprite in self.json["targets"]:
-			if sprite["name"] == sprite_name:
+		for sprite in self.json['targets']:
+			if sprite['name'] == sprite_name:
 				return sprite
-		raise Exception(f"Sprite {sprite_name} is not found")
+		raise KeyError(f'Sprite "{sprite_name}" does not exist')
 
-	def unify_block_ids(self):
-		main_sprite = self.get_sprite("Main")
-		
-		# Append self.name to all block ids
-		main_sprite["blocks"] = {(self.name + ":" if not key.startswith(self.name+':') else '') + key: value for key, value in main_sprite["blocks"].items()}
-		
-		for _, block in main_sprite["blocks"].items():
-			# custom block prototypes
-			if block["opcode"] == "procedures_definition" and not block["inputs"]["custom_block"][1].startswith(self.name+':'):
-				block["inputs"]["custom_block"][1]=self.name+":"+block["inputs"]["custom_block"][1]
-			if block["opcode"] == "procedures_prototype":
-				block["mutation"]["argumentids"] = str([
-					x
-						if x.startswith(self.name+':')
-					else
-						(self.name+':'+x)
-				for x in eval(block["mutation"]["argumentids"])
-				]).replace("'", '"')
-			# Update next pointers
-			if not block["next"] is None and not block["next"].startswith(self.name+':'):
-				block["next"] = self.name + ":" + block["next"]
-			# Update parent pointers
-			if not block["parent"] is None and not block["parent"].startswith(self.name+':'):
-				block["parent"] = self.name + ":" + block["parent"]
-			# Update pointers in block inputs
-			keys = []
-			for key, input in block["inputs"].items():
-				if block["opcode"] == "procedures_prototype":
-					keys.append(key)
-					if input[0] == 1:
-						if type(input[1]) is str and not input[1].startswith(self.name+':'):
-							input[1] = self.name + ":" + input[1]
-				elif input[0] == 3:
-					if type(input[1]) is str and not input[1].startswith(self.name+':'):
-						input[1] = self.name + ":" + input[1]
-					if type(input[2]) is str and not input[2].startswith(self.name+':'):
-						input[2] = self.name + ":" + input[2]
-				elif input[0] == 2:
-					if type(input[1]) is str and not input[1].startswith(self.name+':'):
-						input[1] = self.name + ':' + input[1]
-			for i in keys:
-				if not i.startswith(self.name+':'):
-					block["inputs"][self.name+":"+i] = block["inputs"].pop(i)
+	def make_block_ids_trackable(self):
+		"""
+		assuming that block-ids will be random strings
+		if not already starting with self.id: append self.id to block-id
+		"""
+		sprite = self.get_sprite('Main')
+		# make = make block-id trackable
+		# make for keys
+		sprite['blocks'] = {
+			(self.id+key) if key.count(OMEGA) == 0 else key: value
+			for key, value in sprite['blocks'].items()
+		}
+		for block in sprite['blocks'].values():
+			# make for next, parent
+			if not block['next'] is None and block['next'].count(OMEGA) == 0:
+				block['next'] = self.id+block['next']
+			if not block['parent'] is None and block['parent'].count(OMEGA) == 0:
+				block['parent'] = self.id+block['parent']
+			# make for block inputs
+			for input in block['inputs'].values():
+				if input[0] in (1,2,3) and type(input[1]) is str and input[1].count(OMEGA) == 0:
+					input[1] = self.id+input[1]
+			# make for custom blocks
+			if ( block['opcode'] == 'procedures_definition'
+				 and block['inputs']['custom_block'][1].count(OMEGA) == 0 ):
+				block['inputs']['custom_block'][1] = self.id+block['inputs']['custom_block'][1]
+			if block['opcode'] == 'procedures_prototype':
+				block['mutation']['argumentids'] = str([
+					self.id+x if x.count(OMEGA) == 0 else x
+					for x in eval(block['mutation']['argumentids'])
+				]).replace("'", '"') # FIXME?
+				block['inputs'] = {
+					(self.id+key) if key.count(OMEGA) == 0 else key: value
+					for key, value in block['inputs'].items()
+				}
+				for input in block['inputs'].values():
+					if input[1].count(OMEGA) == 0:
+						input[1] = self.id+input[1]
 
-	def add(self, modl: "Project"):
-		# Fix block-id collisions
-		self.unify_block_ids()
-		modl.unify_block_ids()
-		# Add blocks from modl to self
-		main_sprite = self.get_sprite("Main")
-		modl_sprite = modl.get_sprite("Main")
-		main_sprite["blocks"] = {**main_sprite["blocks"], **modl_sprite["blocks"]}
+	def get_module_blocks(self):
+		return {
+			key: value
+			for key, value in self.get_sprite('Main')['blocks'].items()
+			if key.startswith(self.id)
+		}
 
-	def remove(self, name):
-		main_sprite = self.get_sprite("Main")
-		for i in list(main_sprite["blocks"].keys()):
-			if i.startswith(name+':'):
-				main_sprite["blocks"].pop(i)
+	def get_blocks_except_module(self, module_id):
+		return {
+			key: value
+			for key, value in self.get_sprite('Main')['blocks'].items()
+			if not key.startswith(module_id)
+		}
+
+	def add_module(self, module: 'Project'):
+		sprite = self.get_sprite('Main')
+		sprite['blocks'] = {
+			**self.get_blocks_except_module(module.id),
+			**module.get_module_blocks()
+		}
 	
-	def add_module(self, modl: "Project"):
-		self.remove(modl.name)
-		self.add(modl)
-
-
-main_name = os.getcwd().split('/')[-1]
-
-if os.path.isfile(main_name+'.sb3'):
-	pass
-else:
-	rprint(f'[red] Current folder does not contain {main_name}.sb3 [/red]')
-	exit(1)
-
-
-
-sys.argv = sys.argv[1:]
-
-if len(sys.argv) >= 1:
-	main = Project("MainProject")
-	main.load_project_dir(".")
-	if sys.argv[0] == "add":
-		if len(sys.argv) >= 2:
-			modl = Project(sys.argv[1])
-			try:
-				modl.load_project_dir(modl.name)
-			except:
-				rprint("[red] Module folder not found or does not have .sb3 [/red]")
-				exit(1)
-			main.add_module(modl)
-			main.package()
-		else:
-			rprint("[red] Please enter a module name [/red]")
-			exit(1)
-	elif sys.argv[0] == "remove":
-		if len(sys.argv) >= 2:
-			main.remove(sys.argv[1])
-			main.package()
-		else:
-			rprint("[red] Please enter a module name [/red]")
-			exit(1)
-else:
-	rprint("[red] Please enter a command {add,remove} [/red]")
-	exit(1)
+	def remove_module(self, module_id):
+		sprite = self.get_sprite('Main')
+		sprite['blocks'] = self.get_blocks_except_module(module_id)
